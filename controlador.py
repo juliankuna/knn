c079ef060c6c3ae6bfddf7947f512879b6940a52
Julian Kunaschik <juliankunaschik@gmail.com>
2022-10-03 13:30:53 -0300

    mejorando grafico de puntos del dataset y visualizando por 3 segundos el grafico comparativo de los k entre 1 y 15

--- controlador.py
+++ controlador.py
@@ -8,9 +8,12 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
 import math
-import pandas as pd
+#import pandas as pd
 from modelos import *
-from typing import Any, List
+from typing import Any, List, Counter
+import matplotlib.patches as mpatches
+
+#from knn import *
 
 #(x1,y1) punto de origen, (x2,y2) punto destino
 def CalcularDistanciaEuclidea(x1:float, y1:float, x2:float, y2:float) -> float:
@@ -45,72 +48,245 @@ def GetClasesEnDataSet(todasLasClases)->List[int]:
             listaClases.append(clase)
     return listaClases
 
+def CargarDataSet(datosString:List[str]) -> List[Dato]:
+    dataSet: List[Dato] = []
+
+    n=1             #Eliminamos la primer linea
+    while n<len(datosString):
+        fila=datosString[n].split(',');
+        #cargamos una grilla con cada uno de los puntos (x,y,clase,distancia)
+        dataSet.append(Dato(float(fila[0]), float(fila[1]),int(fila[2])))        
+        n=n+1   
+    return dataSet
+
+#Se dejó de considerar por lo charlado con el profesor Karanik en la clase del 22/09/2022
+def SepararDataSetEnEntrenamientoyPrueba():
+    #contar la cantidad de clases
+    clasesEnElDataset = GetClasesEnDataSet([f.clase for f in dataSet])
+    
+    #Separando todos los puntos del dataset y agrupandolos por su clase
+    clasesConSusPuntos= []
+    for clase in clasesEnElDataset:
+        listaPuntos: list[Punto] = [Punto(f.x,f.y) for f in dataSet if f.clase == clase]
+        clasesConSusPuntos.append(ClaseConPuntos(clase,listaPuntos))
+
+    #Cargando el dataSet de entrenamiento y el de prueba
+    dataSetEntrenamiento.clear()
+    dataSetPrueba:List[Dato] = [] #linea necesaria o sinó el dataSetPrueba.clear() tiraba error
+    dataSetPrueba.clear()
+    for claseConPuntos in clasesConSusPuntos:
+        cantidadPuntos=len(claseConPuntos.puntos)
+        porcentajeEntrenamiento=0.8
+        criterioParada= round(porcentajeEntrenamiento * cantidadPuntos)
+        cont=0
+        for punto in claseConPuntos.puntos:
+            if cont < criterioParada: 
+                dataSetEntrenamiento.append(Dato(float(punto.x),float(punto.y), int(claseConPuntos.clase)))
+                cont+=1
+            else:
+                dataSetPrueba.append(Dato(float(punto.x),float(punto.y), int(claseConPuntos.clase))) 
+
+def BuscarVecinos(k:int, punto:Punto, dataSet:List[Dato]) -> List[dict[int, int, int, list[float]]]:
+        #Obtenemos todas las distancias a los puntos
+        listaDistancias : List[float] = [CalcularDistanciaEuclidea(float(punto.x),float(punto.y), d.x, d.y) for d in dataSet]
+        puntoConDistancias:List[DatoConDistancia] = []
+        i=0
+        for d in dataSet:
+            puntoConDistancias.append( 
+                                {
+                                    "x":d.x,
+                                    "y":d.y,
+                                    "clase":d.clase,
+                                    "distancia":listaDistancias[i]
+                                })
+            i=i+1
+        
+        #ordenamos por proximidad calculada usando la distancia euclidea
+        puntoConDistancias = sorted(puntoConDistancias, key=lambda d : d["distancia"])
+        
+        puntoConDistancias.pop(0) #eliminamos el primero al ser distancia contra el mismo punto
+                
+        #Retornamos los k vecinos mas próximos según la distancia euclidea
+        return puntoConDistancias[:k]
+
+def ClaseMasFrecuente(ListaClasesVecinos) -> int:
+    cantidadClases = Counter(ListaClasesVecinos)   #cantidadClases = Counter({2: 11, 1: 3}) = ({clase: vecesRepetida})
+    ClaseMasRepetida = max(cantidadClases,key=cantidadClases.get)
+    return ClaseMasRepetida
+
+def DefinirClase(punto) -> int:
+        listaVecinos:List[Any] = BuscarVecinos(punto)
+        #listamos las clases de todos los k vecinos
+        ClasesVecinos:List[int] = [n["clase"] for n in listaVecinos] 
+        return ClaseMasFrecuente(ClasesVecinos)
+
+def CargarMatrizPuntos(datosString):
+    #Creamos matriz de valores 0 ceros 
+    matriz= np.zeros((len(datosString)-1,3))
+
+    n=1             #Eliminamos la primer linea
+    while n<len(datosString):
+        fila=datosString[n].split(',');
+        for j in range(2):                
+            matriz [n-1][j] = float(fila[j].strip('\n'))
+        matriz [n-1][2] = int(fila[2].strip('\n'))
+        #cargamos una grilla con cada uno de los puntos (x,y,clase,distancia)
+        n=n+1
+    return matriz   
+
 def IniciarAlgoritmo ():
     try:
-        dataSet.clear()        
-        datosString=CargarArchivo();       
-
-        #Creamos matriz de valores 0 ceros 
-        matriz= np.zeros((len(datosString)-1,3))
-
-        n=1             #Eliminamos la primer linea
-        while n<len(datosString):
-            fila=datosString[n].split(',');
-            for j in range(2):                
-                matriz [n-1][j] = float(fila[j].strip('\n'))
-            matriz [n-1][2] = int(fila[2].strip('\n'))
-            #cargamos una grilla con cada uno de los puntos (x,y,clase,distancia)
-            dataSet.append(Dato(float(fila[0]), float(fila[1]),int(fila[2])))        
-            n=n+1   
-        #contar la cantidad de clases
-        clasesEnElDataset = GetClasesEnDataSet([f.clase for f in dataSet])
-
-        #Separando todos los puntos del dataset y agrupandolos por su clase
-        clasesConSusPuntos= []
-        for clase in clasesEnElDataset:
-            listaPuntos: list[Punto] = [Punto(f.x,f.y) for f in dataSet if f.clase == clase]
-            clasesConSusPuntos.append(ClaseConPuntos(clase,listaPuntos))
-
-        #Cargando el dataSet de entrenamiento y el de prueba
-        dataSetEntrenamiento.clear()
-        dataSetPrueba:List[Dato] = [] #linea necesaria o sinó el dataSetPrueba.clear() tiraba error
-        dataSetPrueba.clear()
-        for claseConPuntos in clasesConSusPuntos:
-            cantidadPuntos=len(claseConPuntos.puntos)
-            porcentajeEntrenamiento=0.8
-            criterioParada= round(porcentajeEntrenamiento * cantidadPuntos)
-            cont=0
-            for punto in claseConPuntos.puntos:
-                if cont < criterioParada: 
-                    dataSetEntrenamiento.append(Dato(float(punto.x),float(punto.y), int(claseConPuntos.clase)))
-                    cont+=1
-                else:
-                    dataSetPrueba.append(Dato(float(punto.x),float(punto.y), int(claseConPuntos.clase)))
+        dataSet: List[Dato] = []
+        datosString= CargarArchivo();
+        dataSet= CargarDataSet(datosString)
+        matriz=CargarMatrizPuntos(datosString)
             
-        
+        #Obtener k optimo.
+        valorKOptimo.set(ObtenerKOptimo(dataSet))
         
         #GENERAR GRILLA
-        GenerarGrilla()   
+        #GenerarGrilla()   
     except Exception as e:
         messagebox.showwarning(message="Houston, we have a problem..." + str(e), title="Alerta")
+        print(str(e))
         return
     #Filtro de color
     color_filt=[]
-    color = ['red','green','blue']
+    colors = ['red','green','blue']
     for line in matriz:
         if line[2]==0:
-            color_filt.append(color[0])
+            color_filt.append(colors[0])
         elif line[2]==1:
-            color_filt.append(color[1])
+            color_filt.append(colors[1])
         elif line[2]==2:
-            color_filt.append(color[2])
+            color_filt.append(colors[2])
 
+    # plt.close()
+    # plt.scatter(matriz[:,0],matriz[:,1],color=color_filt)
+    # plt.xlabel('Eje x')
+    # plt.ylabel('Eje y')
+    # plt.title('Gráfico de puntos con k='+str(valorK.get()))
+    # plt.show()
+    clasesDelDataset=GetClasesEnDataSet([f.clase for f in dataSet])
+    patches= []
+    for i in range(0,len(clasesDelDataset)):
+        patch = mpatches.Patch(color=colors[i], label=f'Clase: {clasesDelDataset[i]}')
+        patches.append(patch)
     plt.close()
-    plt.scatter(matriz[:,0],matriz[:,1],color=color_filt)
+    plt.scatter(matriz[:,0],matriz[:,1],color=color_filt,label="Datos")
     plt.xlabel('Eje x')
     plt.ylabel('Eje y')
-    plt.title('Gráfico de puntos con k='+str(valorK.get()))
+    plt.title('Gráfico de puntos del dataset')
+    
+    plt.legend(handles=patches)
+    #plt.legend(loc='best')
     plt.show()
+
+def ObtenerKOptimo(dataSet:List[Dato]) -> int:
+    KFoldResults = []
+    PuntosConDist = []
+    #cargamos la tabla en horizontal de cada punto con sus 9 vecinos
+    dataSetPruebaCalculado = []
+    for d in dataSet:
+        #este es el que va a sobreescribir los valores de la clase de cada punto y 
+        # de ahi vamos a comparar contra el dataSetPrueba que tiene los valores reales        
+        dataSetPruebaCalculado.append(d) 
+        PuntosConDist.append(BuscarVecinos(len(dataSet)-1,Punto(d.x,d.y),dataSet)) 
+    #PuntosConDistancias tiene la distancia los k puntos vecinos en el dataset
+
+    #contador de aciertos para los k-vecinos
+    contadorAciertos = 0
+    #Desde k=1 hasta 15
+    for k in range(1,15):
+        #recorremos una columna y guardamos primero el punto cabecera,
+        #y luego una lista de solo las etiquetas de sus vecinos
+        PuntosConDistancias=PuntosConDist
+        i=0
+        for puntoConTodosLosVecinos in PuntosConDistancias:
+            puntoskVecinos=puntoConTodosLosVecinos[:k]
+            ClasesDeVecinos:List[int] = []
+            dato = dataSetPruebaCalculado[i]
+
+            # for puntoVecino in puntoskVecinos:
+            #     ClasesDeVecinos.append(puntoVecino['clase'])
+            ClasesDeVecinos = [p['clase'] for p in puntoskVecinos]
+            ClaseMasRepetida = 0
+            #determinamos cada clase y su cantidad de ocurrencias en los k vecinos
+            cantidadClases = Counter(ClasesDeVecinos)
+            #determinamos el valor de la clase para el punto analizado en base a sus vecinos
+            ClaseMasRepetida = max(cantidadClases,key=cantidadClases.get)
+            
+            isPonderado=False
+            if(isPonderado):
+                clasesyPesos:List[ClaseYDistancia] = []
+                for clase in ClasesDeVecinos:
+                    acu=0
+                    for p in puntoskVecinos:
+                        if(p['clase'] == clase):
+                            distanciaEuclidea=(CalcularDistanciaEuclidea(p['x'],p['y'],dato.x,dato.y))
+                            if(distanciaEuclidea > 0 ):
+                                acu=acu+1*(1/(distanciaEuclidea**2))
+                                clasesyPesos.append({
+                                                "clase": clase,
+                                                "peso":acu
+                                            })
+                
+                clasesyPeso = sorted(clasesyPesos, key=lambda d : d["peso"])
+                ClaseMasRepetida = clasesyPesos[0]['clase']             
+            else:
+                print('Camino del kNN tradicional')    
+                            
+            ocurrenciasDeMaxValRep = 0
+            CantidadDeVecesDeLaClaseMasRepetida = cantidadClases[ClaseMasRepetida]
+            for clase in cantidadClases:
+                if cantidadClases[clase] == CantidadDeVecesDeLaClaseMasRepetida:
+                    ocurrenciasDeMaxValRep=ocurrenciasDeMaxValRep+1
+            if ocurrenciasDeMaxValRep == 1:
+                if ClaseMasRepetida == dato.clase:
+                    contadorAciertos = contadorAciertos+1
+            ocurrenciasDeMaxValRep = 0
+        KFoldResults.append({"k":k, "Presición":contadorAciertos})
+        contadorAciertos = 0
+        i+=1
+    
+    #PLOT AND GUI TABLE FUNCTIONS
+    xList:List[int] = [x["k"] for x in KFoldResults]
+    yList:List[int] = [y["Presición"] for y in KFoldResults]
+    # for KFold in KFoldResults:
+    #     xList.append(KFold["k"])
+    #     yList.append(KFold["accuracy"])
+    
+    KFoldResults = sorted(KFoldResults, reverse=True, key=lambda d : d["Presición"])
+    kOptimo = KFoldResults[0]
+
+    plt.plot(xList,yList,'r--')
+    plt.title(f"Comparación de valores k (Óptimo: k={kOptimo['k']}, Cantidad de aciertos = {kOptimo['Presición']})")
+    plt.xlabel('Valor de k')
+    plt.ylabel('Aciertos')
+
+    toShow:List[Any] = []
+    for KFold in KFoldResults:
+        if KFold["Presición"] == kOptimo["Presición"]:
+            toShow.append([KFold["k"],KFold["Presición"]])
+
+    plt.plot(kOptimo["k"], kOptimo["Presición"], color='green', marker="x")
+
+    plt.annotate(f'Mas preciso: ({kOptimo["k"]};{kOptimo["Presición"]})',
+                xy=(kOptimo["k"], kOptimo["Presición"]),
+                xytext=(kOptimo["k"], kOptimo["Presición"]/2),
+                arrowprops=dict(facecolor='black', arrowstyle="->"))
+
+    #Cargamos la tabla con los resultados de Kfold
+    # gui.kfold_results_table.delete(*gui.kfold_results_table.get_children())
+    # for bettersKinfo in toShow:
+    #     gui.kfold_results_table.insert("", 'end', values=bettersKinfo)
+    # gui.kfold_results_table.pack(pady=10)
+
+    plt.ion()
+    plt.show()
+    plt.pause(3)
+    return kOptimo
+
 
 def setPathFile():
     nombreConRuta = filedialog.askopenfilename(title="Elegir un DataSet",filetypes = (("excel files",".csv"),("all files",".*")));
@@ -144,6 +320,8 @@ def GraficarVistaInicial ():
 
     botonIniciar = Button(frame1, text='Iniciar algoritmo',command=IniciarAlgoritmo)
     botonIniciar.grid(row=6,column=1,sticky='e',padx=0,pady=10)
+    botonObtenerKOptimo = Button(frame1, text='Obtener k óptimo',command=ObtenerKOptimo)
+    botonObtenerKOptimo.grid(row=7,column=1,sticky='e',padx=0,pady=10)
     buttonDataSet = Button(frame1, text = "Seleccionar DATASET", width=15,command = setPathFile)
     buttonDataSet.grid(row=1,column=1,sticky='e',padx=5,pady=15)
 
@@ -152,44 +330,7 @@ def GraficarVistaInicial ():
 
 #/////////////////////////////////////////////////////////////////////////////////////////////////////
 #/////////////////////////////////////////////////////////////////////////////////////////////////////
-def BuscarVecinos(punto:Punto) -> List[dict[int, int, int, list[float]]]:
-        #Obtenemos todas las distancias a los puntos
-        listaDistancias : List[float] = [CalcularDistanciaEuclidea(float(punto[0]),float(punto[1]), d.x, d.y) for d in dataSetEntrenamiento]
-        puntoConDistancias:List[DatoConDistancia] = []
-        i=0
-        for d in dataSetEntrenamiento:
-            puntoConDistancias.append( 
-                                {
-                                    "x":d.x,
-                                    "y":d.y,
-                                    "clase":d.clase,
-                                    "distancia":listaDistancias[i]
-                                })
-            i=i+1
-        
-        #ordenamos por proximidad calculada usando la distancia euclidea
-        puntoConDistancias = sorted(puntoConDistancias, key=lambda d : d["distancia"])
-        
-        puntoConDistancias.pop(0) #eliminamos el primero al ser distancia contra el mismo punto
-        
-        #Retornamos los k vecinos mas próximos según la distancia euclidea
-        return puntoConDistancias[:valorK.get()]
 
-def ClaseMasFrecuente(List) -> int:
-    masFrecuente = 0
-    clase = List[0]        
-    for i in List:
-        frecuenciaDeLaClase = List.count(i)
-        if(frecuenciaDeLaClase > masFrecuente):
-            masFrecuente = frecuenciaDeLaClase
-            clase = i
-    return clase
-
-def DefinirClase(punto) -> int:
-        listaVecinos:List[Any] = BuscarVecinos(punto)
-        #listamos las clases de todos los k vecinos
-        ClasesVecinos:List[int] = [n["clase"] for n in listaVecinos] 
-        return ClaseMasFrecuente(ClasesVecinos)
 
 def GenerarGrilla():
         #Definiendo maximos y minimos para la grilla
@@ -227,9 +368,9 @@ def GenerarGrilla():
                     label="Grilla")
 
         #Gráfico del dataset
-        GrillaConDataset.scatter([d.x for d in dataSetEntrenamiento],
-                    [d.y for d in dataSetEntrenamiento],
-                    c = [d.clase for d in dataSetEntrenamiento],
+        GrillaConDataset.scatter([d.x for d in dataSet],
+                    [d.y for d in dataSet],
+                    c = [d.clase for d in dataSet],
                     alpha = 0.9,
                     cmap= "tab10",
                     marker="X",
@@ -259,10 +400,6 @@ root=Tk()
 root.title('Algoritmo kNN - Kunaschik, Saucedo, Zitelli');
 root.resizable(0,0)
 root.geometry('750x450')
-
-dataSet: List[Dato] = []
-dataSetPrueba:List[Dato] = []
-dataSetEntrenamiento: List[Dato] = []
 
 clasesConSusPuntos:List[ClaseConPuntos] = []
 
